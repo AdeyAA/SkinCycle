bde00bd08fb4ba082a80d1cff51fd8fe55e7fd3f
AdeyAA <96445037+AdeyAA@users.noreply.github.com>
2024-02-13 19:59:32 -0800

    Fixed some more login stuff

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 db = SQLAlchemy(app)
 
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
